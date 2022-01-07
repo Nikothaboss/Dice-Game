c6c1e5a81233a31636346f00d2348b72f1a55a8c
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-01-07 20:45:15 +0100

    fixed: game logic, added: reset function, todo: winner function

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,63 @@
 import { useState } from "react";
 
-// Lag en funksjon som retunerer et tall mellom 1-6
-
-
-
 function App() {
   const [currentPlayer, setCurrenPlayer] = useState(null)
+  const [currentRoll, setCurrentRoll] = useState("?")
   const [num, setNum] = useState(0);
   const [num2, setNum2] = useState(0);
   
   function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
   }
-  
-  const rollDice = () => {
-    const randomNumber = Math.floor(getRandomNumber(1, 7))
-    const randomDeducter = Math.floor(getRandomNumber(1, 10))
-    currentPlayer === "Player 1" ? setNum(num + randomNumber) : setNum2(num2 + randomNumber)
-    
-    
-    if(randomNumber === 6){
-      const confirm = window.confirm("You rolled 6, press to go again")
-      if(confirm){
-        currentPlayer === "Player 1" ? setNum(num + randomNumber) : setNum2(num2 + randomNumber)
-      }
-    }else if(randomNumber === 1){
-      const confirm = window.confirm(`You rolled 1, you lose ${randomDeducter}`);
-      if(confirm){
-        currentPlayer === "Player 1" ? setNum(num - randomDeducter) : setNum2(num2 - randomDeducter)
-      }
-    }
-    console.log(randomNumber)
-  }
 
+  // Velger en random spiller til å begynne gamet
   const startGame = () => {
     const startingPlayer = Math.floor(getRandomNumber(1, 3))
     if(startingPlayer === 1){
-      setCurrenPlayer("Player 1")
+      setCurrenPlayer(true)
     }else if(startingPlayer === 2){
-      setCurrenPlayer("Player 2")
+      setCurrenPlayer(false)
     }
+  }
+  
+  // Ruller terningen og oppdaterer poengsummen
+  const rollDice = () => {
+    const randomNumber = Math.floor(getRandomNumber(1, 7))
+    const randomDeducter = Math.floor(getRandomNumber(1, 10))
+
+    // Displayer hvilket tall som ble rullet 
+    setCurrentRoll(randomNumber)
+
+    // Setter en delay på oppdateringen av Poengsummen på 1000ms/1sek + resetter currentRoll state til "?"
+    setTimeout(()=>{
+      if(randomNumber === 6){
+        const confirm = window.confirm("You rolled 6, press to go again")
+        if(confirm){
+          currentPlayer === true ? setNum(num + randomNumber) : setNum2(num2 + randomNumber)
+        }
+      }else if(randomNumber === 1){
+        const confirm = window.confirm(`You rolled 1, you lose ${randomDeducter} points`);
+        if(confirm){
+          currentPlayer === true ? setNum(num - randomDeducter) : setNum2(num2 - randomDeducter)
+          setCurrenPlayer(!currentPlayer)
+        }
+      }else{
+          currentPlayer ? setNum(num + randomNumber) : setNum2(num2 + randomNumber)
+          setCurrenPlayer(!currentPlayer)
+      }
+
+      setCurrentRoll("?")
+    }, 1000)
+    
+    
+  }
+
+  // Resetter spillet
+  const reset = () => {
+    setNum(0)
+    setNum2(0)
+    setCurrenPlayer(null)
+    setCurrentRoll("?")
   }
 
 
@@ -48,7 +66,9 @@ function App() {
     <div className="App">
       <button onClick={startGame}>Start Game</button>
       <button onClick={rollDice}>Roll</button>
-      <h1>Current Player: {currentPlayer}</h1>
+      <button onClick={reset}>Reset</button>
+      <h1>Current Player: {currentPlayer === null ? "Player ?" : currentPlayer ? "Player 1" : "Player 2"}</h1>
+      <h1>Rolled: {currentRoll}</h1>
       <h1>Player 1 score: {num}</h1>
       <h1>Player 2 score: {num2}</h1>
     </div>
